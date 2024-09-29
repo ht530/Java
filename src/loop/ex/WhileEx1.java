5fc8f7c255b73fb97b38e6ae447dd121d654644f
ht530 <ht530@naver.com>
2024-09-29 09:06:01 +0900

    While, For문 문제풀이

---
+++ src/loop/ex/WhileEx1.java
@@ -0,0 +1,13 @@
+package loop.ex;
+
+public class WhileEx1 {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        while(count<10){
+            count ++;
+            System.out.println(count);
+        }
+    }
+}
